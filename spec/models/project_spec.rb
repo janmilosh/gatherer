77fca824f559d5cc0ec7659e57ce7a055b23422b
janmilosh <janmilosh@gmail.com>
2015-03-26 21:52:01 -0400

    Adds some project refactoring and failing velocity tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe "initialization" do
-    let(:project) { Project.new }
-    let(:task) { Task.new }
+  let(:project) { Project.new }
+  let(:task) { Task.new }
 
   it "considers a project with no tasks to be done" do
     expect(project.done?).to be_truthy
@@ -17,5 +17,24 @@ RSpec.describe "initialization" do
     project.tasks << task
     task.mark_completed
     expect(project).to be_done
+  end
+end
+
+describe "estimates" do
+  let(:project) { Project.new }
+  let(:done) { Task.new(size: 2, completed: true) }
+  let(:small_not_done) { Task.new(size: 1) }
+  let(:large_not_done) { Task.new(size:4 ) }
+
+  before(:example) do
+    project.tasks = [done, small_not_done, large_not_done]
+  end
+
+  it "can calculate total size" do
+    expect(project.total_size).to eq(7)
+  end
+
+  it "can calculate remaining size" do
+    expect(project.remaining_size).to eq(5)
   end
 end
