25b5a43a463f5f27ac6706871fe32edf49e9e292
janmilosh <janmilosh@gmail.com>
2015-03-29 17:29:51 -0400

    Adds Active Record to models, starts e2e test

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,9 @@
-class Project
-  attr_accessor :tasks
+class Project < ActiveRecord::Base
+  
+  has_many :tasks
 
-  def initialize
-    @tasks = []
+  def self.velocity_length_in_days
+    21
   end
 
   def incomplete_tasks
@@ -14,11 +15,27 @@ class Project
   end
 
   def total_size
-    tasks.sum(&:size)
+    tasks.to_a.sum(&:size)
   end
 
   def remaining_size
     incomplete_tasks.sum(&:size)
   end
 
+  def completed_velocity
+    tasks.to_a.sum(&:points_toward_velocity)
+  end
+
+  def current_rate
+    completed_velocity * 1.0/Project.velocity_length_in_days
+  end
+
+  def projected_days_remaining
+    remaining_size/current_rate
+  end
+
+  def on_schedule?
+    return false if projected_days_remaining.nan?
+    (Date.today + projected_days_remaining) <= due_date
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,16 +1,21 @@
-class Task
-  attr_accessor :size, :completed
+class Task < ActiveRecord::Base
+  
+  belongs_to :project
 
-  def initialize(options = {})
-    @completed = options[:completed]
-    @size = options[:size]
-  end
-
-  def mark_completed
-    @completed = true
+  def mark_completed(date = nil)
+    self.completed_at = (date || Time.current)
   end
 
   def complete?
-    @completed
+    self.completed_at.present?
+  end
+
+  def part_of_velocity?
+    return false unless complete?
+    completed_at > Project.velocity_length_in_days.days.ago
+  end
+
+  def points_toward_velocity
+    if part_of_velocity? then size else 0 end
   end
 end
